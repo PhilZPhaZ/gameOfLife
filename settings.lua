11247f7b5faa4341e6da2afc85dc7f978fb971b5
Phileas Bour <bour.phileas@gmail.com>
2024-09-10 10:19:19 +0200

    removed settings message

--- settings.lua
+++ settings.lua
@@ -9,12 +9,7 @@ end
 function settings.draw()
     love.graphics.setColor(217, 217, 217)
     love.graphics.print('Paramètre', 100, 100)
-    love.graphics.print('Touches', 100, 200)
-    love.graphics.print('Echap : Retour au menu', 100, 250)
-    love.graphics.print('Espace / flèche droite : Générer une nouvelle génération', 100, 300)
-    love.graphics.print('Entrée / retour arrière : Effacer la grille', 100, 350)
-    love.graphics.print('G : Activer/désactiver la génération automatique', 100, 400)
-    love.graphics.print('R : Remplir la grille aléatoirement', 100, 450)
+    love.graphics.print('Pas encore implémenté', 100, 200)
 end
 
 return settings
